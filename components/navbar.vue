74f077708ff6ab0c6576830e09154da13b3c4646
Archie ALLEN <allea302@woodbridgehigh.net>
2025-07-11 12:03:51 +0000

    Initial commit

---
+++ components/navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="bg-gradient-to-r from-red-400 to-red-700 bg-opacity-75 p-4 shadow-lg">
+    <div class="container mx-auto flex flex-col md:flex-row items-center justify-between">
+      <div class="flex items-center space-x-4">
+        <NuxtImg src="/icon.png" height="50" class="rounded-full" />
+        <span class="text-white text-xl font-bold">Archies Festival</span>
+      </div>
+      <div class="hidden md:flex items-center space-x-6">
+        <a href="" class="text-gray-300 hover:text-white transition duration-300">Home</a>
+      <a href="tickets" class="text-gray-300 hover:text-white transition duration-300">Tickets</a>
+      </div>
+      <button class="md:hidden text-white focus:outline-none" @click="toggleMenu">
+        <Icon name="heroicons-outline:menu" size="24" />
+      </button>
+    </div>
+    <div v-if="menuOpen" class="md:hidden mt-4 flex flex-col space-y-2">
+      <a href="" class="text-gray-300 hover:text-white transition duration-300">Home</a>
+      <a href="tickets" class="text-gray-300 hover:text-white transition duration-300">Tickets</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.menuOpen = !this.menuOpen;
+    },
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="bg-gradient-to-r from-orange-400 to-red-400 min-h-screen text-white">
+        <div class="text-center font-mono text-white text-5xl">Archies Festival</div>
+        <div class="bg-gray-800 h-64 w-64 rounded-4xl mb-4 drop-shadow-lg hover:scale-105 transition-transform duration-300"></div>
+    </div>
+</template>
